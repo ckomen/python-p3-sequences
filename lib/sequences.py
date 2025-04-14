d4b645eb4cd0cf80916222d937be317e0a9eb7d0
ckomen <cjepkorir09@gmail.com>
2025-04-14 20:44:43 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    sequence = [0, 1][:length] 
+    for i in range(2, length):
+        sequence.append(sequence[-1] + sequence[-2])
+    print(sequence)
     pass
+
+   
